47e17e36c3266d12f81185eb2fa9f5a182d083fd
Dominik Weikert <dominik.weikert@ovgu.de>
2020-07-02 14:42:57 +0200

    added ns3 install script

---
+++ installns3.sh
@@ -0,0 +1,20 @@
+#git sibmodule setup
+git checkout NS-3-integration
+git submodule init
+git submodule update 
+
+#ns-3 utilities download
+python3 download.py
+
+
+
+curdir=$(pwd)
+cd ns3
+./waf configure --build-profile=debug --enable-examples --enable-tests
+./waf clean
+./waf -v
+cd $curdir
+
+
+#TODO: different build config support
+
